ae4c5d12c9177c217215ea30908291b58fdc0296
ivan.hoyos <ivan.hoyos@rappi.com>
2025-01-06 11:12:34 -0500

    ETL process from API to data warehousing

--- scripts/main.py
+++ scripts/main.py
@@ -5,32 +5,7 @@ def exchange_rate():
 
     load_dotenv()
 
-    country_currency_code = {
-        'México': 'MXN',
-        'Guatemala': 'GTQ',
-        'Belice': 'BZD',
-        # 'El Salvador': 'USD',
-        'Honduras': 'HNL',
-        'Nicaragua': 'NIO',
-        'Costa Rica': 'CRC',
-        'Panamá': 'PAB',
-        'Colombia': 'COP',
-        'Venezuela': 'VES',
-        # 'Ecuador': 'USD',
-        'Perú': 'PEN',
-        'Bolivia': 'BOB',
-        'Paraguay': 'PYG',
-        'Chile': 'CLP',
-        'Argentina': 'ARS',
-        'Uruguay': 'UYU',
-        'Brasil': 'BRL',
-        'Cuba': 'CUP',
-        'República Dominicana': 'DOP',
-        # 'Puerto Rico': 'USD',
-        'Haití': 'HTG',
-    }
-
-    fecha = '2024-01-01'
+    fecha = datetime.now().date()
     API_KEY = os.getenv('ENV_API_KEY')
     BASE_URL = 'http://api.currencylayer.com/'
     URL_LIVE = f'{BASE_URL}live?access_key={API_KEY}'
@@ -38,14 +13,14 @@ def exchange_rate():
 
     URL = URL_LIVE
 
-    # Hacemos la solicitud a la API
+    # Request to API. Extact.
     response = requests.get(URL)
     data = response.json()
     data = data['quotes']
 
     renamed_data = [{"currency_code": key, "amount": value} for key, value in data.items()]
 
-    # Convertir a DataFrame
+    # Transform dataframe
     df = pd.DataFrame(renamed_data)
     df['exchange_at'] = datetime.now().date()
     
@@ -54,12 +29,11 @@ def exchange_rate():
 
 def snowflake_load():
     import snowflake.connector
-    from sqlalchemy import create_engine
 
     load_dotenv()
     data = exchange_rate()
 
-    # Configuración de conexión a Snowflake
+    # Config snowflake connection
     sf_account = os.getenv('ENV_SF_ACCOUNT')
     sf_user = os.getenv('ENV_SF_USER')
     sf_password = os.getenv('ENV_SF_PASSWORD')
@@ -67,7 +41,7 @@ def snowflake_load():
     sf_database = os.getenv('ENV_SF_DATABASE')
     sf_schema = os.getenv('ENV_SF_SCHEMA')
 
-    # Crear la conexión con Snowflake
+    # Create snowflake connection
     conn = snowflake.connector.connect(
         user=sf_user,
         password=sf_password,
@@ -77,20 +51,20 @@ def snowflake_load():
         schema=sf_schema
     )
 
-    # Insertar datos en la tabla
+    # open transaction
     cursor = conn.cursor()
 
-    # Generar el comando INSERT y ejecutarlo por cada fila del DataFrame
+    # Insert data from each row of the dataframe. Load.
     insert_sql = f"INSERT INTO PHYGITAL_PROD.BRONZE_LAYER.GLOBAL_EXCHANGE_RATE (currency_code, amount, exchange_at) VALUES (%s, %s, %s)"
     for index, row in data.iterrows():
         cursor.execute(insert_sql, (row['currency_code'], row['amount'], row['exchange_at']))
 
-    # Confirmar la transacción y cerrar conexión
+    # Confirm and close transaction
     conn.commit()
     cursor.close()
     conn.close()
 
-    print("Datos insertados exitosamente en Snowflake.")
+    print("Data successfully inserted.")
 
 
 if __name__ == '__main__':
